3bf026736988f90453ac22827b32d3d164c742d0
Vincent Donnefort <vincent.donnefort@arm.com>
2020-08-13 19:24:52 +0100

    ANDROID: sched/fair: update_max_interval() active_mask dependant
    Adding support in update_max_interval() for incomplete HP _cpu_down, where
    cpu_active_mask != cpu_online_mask. This situation can happen in the event
    of a partial _cpu_down. i.e. _cpu_down(target) where
    target > CPUHP_AP_OFFLINE.
    
    Bug: 161210528
    Change-Id: Ia422057c65f16dc9aa8f6d272098b2308b00f0ac
    Signed-off-by: Vincent Donnefort <vincent.donnefort@arm.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6846,6 +6846,8 @@ int sched_cpu_activate(unsigned int cpu)
 	}
 	rq_unlock_irqrestore(rq, &rf);
 
+	update_max_interval();
+
 	return 0;
 }
 
@@ -6880,6 +6882,9 @@ int sched_cpu_deactivate(unsigned int cpu)
 		return ret;
 	}
 	sched_domains_numa_masks_clear(cpu);
+
+	update_max_interval();
+
 	return 0;
 }
 
@@ -6888,7 +6893,6 @@ static void sched_rq_cpu_starting(unsigned int cpu)
 	struct rq *rq = cpu_rq(cpu);
 
 	rq->calc_load_update = calc_load_update;
-	update_max_interval();
 }
 
 int sched_cpu_starting(unsigned int cpu)
@@ -6917,7 +6921,6 @@ int sched_cpu_dying(unsigned int cpu)
 	rq_unlock_irqrestore(rq, &rf);
 
 	calc_load_migrate(rq);
-	update_max_interval();
 	nohz_balance_exit_idle(rq);
 	hrtick_clear(rq);
 	return 0;
