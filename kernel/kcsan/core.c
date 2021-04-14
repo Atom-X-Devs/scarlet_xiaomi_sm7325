ea7aa20ea5581d8dfa71d4e5fa920c1f024ee111
Mark Rutland <mark.rutland@arm.com>
2021-04-14 13:28:18 +0200

    kcsan: Distinguish kcsan_report() calls
    Currently kcsan_report() is used to handle three distinct cases:
    
    * The caller hit a watchpoint when attempting an access. Some
      information regarding the caller and access are recorded, but no
      output is produced.
    
    * A caller which previously setup a watchpoint detected that the
      watchpoint has been hit, and possibly detected a change to the
      location in memory being watched. This may result in output reporting
      the interaction between this caller and the caller which hit the
      watchpoint.
    
    * A caller detected a change to a modification to a memory location
      which wasn't detected by a watchpoint, for which there is no
      information on the other thread. This may result in output reporting
      the unexpected change.
    
    ... depending on the specific case the caller has distinct pieces of
    information available, but the prototype of kcsan_report() has to handle
    all three cases. This means that in some cases we pass redundant
    information, and in others we don't pass all the information we could
    pass. This also means that the report code has to demux these three
    cases.
    
    So that we can pass some additional information while also simplifying
    the callers and report code, add separate kcsan_report_*() functions for
    the distinct cases, updating callers accordingly. As the watchpoint_idx
    is unused in the case of kcsan_report_unknown_origin(), this passes a
    dummy value into kcsan_report(). Subsequent patches will refactor the
    report code to avoid this.
    
    There should be no functional change as a result of this patch.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    [ elver@google.com: try to make kcsan_report_*() names more descriptive ]
    Signed-off-by: Marco Elver <elver@google.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/kcsan/core.c
+++ kernel/kcsan/core.c
@@ -365,9 +365,7 @@ static noinline void kcsan_found_watchpoint(const volatile void *ptr,
 	flags = user_access_save();
 
 	if (consumed) {
-		kcsan_report(ptr, size, type, KCSAN_VALUE_CHANGE_MAYBE,
-			     KCSAN_REPORT_CONSUMED_WATCHPOINT,
-			     watchpoint - watchpoints);
+		kcsan_report_set_info(ptr, size, type, watchpoint - watchpoints);
 	} else {
 		/*
 		 * The other thread may not print any diagnostics, as it has
@@ -537,8 +535,8 @@ kcsan_setup_watchpoint(const volatile void *ptr, size_t size, int type)
 		if (is_assert && value_change == KCSAN_VALUE_CHANGE_TRUE)
 			atomic_long_inc(&kcsan_counters[KCSAN_COUNTER_ASSERT_FAILURES]);
 
-		kcsan_report(ptr, size, type, value_change, KCSAN_REPORT_RACE_SIGNAL,
-			     watchpoint - watchpoints);
+		kcsan_report_known_origin(ptr, size, type, value_change,
+					  watchpoint - watchpoints);
 	} else if (value_change == KCSAN_VALUE_CHANGE_TRUE) {
 		/* Inferring a race, since the value should not have changed. */
 
@@ -547,9 +545,7 @@ kcsan_setup_watchpoint(const volatile void *ptr, size_t size, int type)
 			atomic_long_inc(&kcsan_counters[KCSAN_COUNTER_ASSERT_FAILURES]);
 
 		if (IS_ENABLED(CONFIG_KCSAN_REPORT_RACE_UNKNOWN_ORIGIN) || is_assert)
-			kcsan_report(ptr, size, type, KCSAN_VALUE_CHANGE_TRUE,
-				     KCSAN_REPORT_RACE_UNKNOWN_ORIGIN,
-				     watchpoint - watchpoints);
+			kcsan_report_unknown_origin(ptr, size, type);
 	}
 
 	/*
