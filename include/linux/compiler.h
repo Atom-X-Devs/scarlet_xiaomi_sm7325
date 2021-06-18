571675d1cb5fe8ed958167373ec83bc65007ac35
Kees Cook <keescook@chromium.org>
2021-06-17 22:48:05 -0700

    BACKPORT: compiler_types.h: Remove __compiletime_object_size()
    Since all compilers support __builtin_object_size(), and there is only
    one user of __compiletime_object_size, remove it to avoid the needless
    indirection. This lets Clang reason about check_copy_size() correctly.
    
    Link: https://github.com/ClangBuiltLinux/linux/issues/1179
    Suggested-by: Nick Desaulniers <ndesaulniers@google.com>
    Cc: Nathan Chancellor <nathan@kernel.org>
    Cc: Nick Desaulniers <ndesaulniers@google.com>
    Cc: Sedat Dilek <sedat.dilek@gmail.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Marco Elver <elver@google.com>
    Cc: Arvind Sankar <nivedita@alum.mit.edu>
    Cc: Masahiro Yamada <masahiroy@kernel.org>
    Cc: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Sami Tolvanen <samitolvanen@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Gabriel Krisman Bertazi <krisman@collabora.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Reviewed-by: Miguel Ojeda <ojeda@kernel.org>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -381,10 +381,6 @@ static inline void *offset_to_ptr(const int *off)
 
 #endif /* __ASSEMBLY__ */
 
-/* Compile time object size, -1 for unknown */
-#ifndef __compiletime_object_size
-# define __compiletime_object_size(obj) -1
-#endif
 #ifndef __compiletime_warning
 # define __compiletime_warning(message)
 #endif
