d504fe41a1e6690cee183ebf0d39d65b19c063b7
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-05-17 18:51:28 +0600

    usb: gadget: f_fs: Make ipc log strings no-op
    We don't necessary need ipc log string whereas ipc logging is
    disabled at defconfig anyway. Make these no-op.
    
    This fixes the following warning by GCC (in an unorthodox way):
    
    ../drivers/usb/gadget/function/f_fs.c: In function 'ffs_func_eps_disable':
    ../drivers/usb/gadget/function/f_fs.c:50:9: warning: 'ffs' is used uninitialized [-Wuninitialized]
       50 |         ipc_log_string(ffs->ipc_log, "%s: " fmt,  __func__, ##__VA_ARGS__)
          |         ^~~~~~~~~~~~~~
    ../drivers/usb/gadget/function/f_fs.c:2095:26: note: 'ffs' was declared here
     2095 |         struct ffs_data *ffs;
          |                          ^~~
    ../drivers/usb/gadget/function/f_fs.c: In function 'ffs_func_eps_enable':
    ../drivers/usb/gadget/function/f_fs.c:50:9: warning: 'ffs' is used uninitialized [-Wuninitialized]
       50 |         ipc_log_string(ffs->ipc_log, "%s: " fmt,  __func__, ##__VA_ARGS__)
          |         ^~~~~~~~~~~~~~
    ../drivers/usb/gadget/function/f_fs.c:2126:26: note: 'ffs' was declared here
     2126 |         struct ffs_data *ffs;
          |                          ^~~
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -63,12 +63,10 @@ static inline void ffs_ipc_log_destroy(struct ffs_data *ffs)
 
 #ifdef CONFIG_DYNAMIC_DEBUG
 #define ffs_log(fmt, ...) do { \
-	ipc_log_string(ffs->ipc_log, "%s: " fmt,  __func__, ##__VA_ARGS__); \
 	dynamic_pr_debug("%s: " fmt, __func__, ##__VA_ARGS__); \
 } while (0)
 #else
-#define ffs_log(fmt, ...) \
-	ipc_log_string(ffs->ipc_log, "%s: " fmt,  __func__, ##__VA_ARGS__)
+#define ffs_log(fmt, ...) ((void)0)
 #endif
 #else
 static inline void ffs_ipc_log_create(struct ffs_data *ffs,
