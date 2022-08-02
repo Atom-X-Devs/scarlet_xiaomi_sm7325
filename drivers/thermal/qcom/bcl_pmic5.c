f872e03e8d20a6fb9918de85b9a2e6200f141a4e
Divyanshu-Modi <divyan.m05@gmail.com>
2022-08-02 09:57:20 +0530

    treewide: Remove IPC logging support
    Change-Id: Ifc01575058c7697feb06f12fbcdb7fd0a95951ed
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/thermal/qcom/bcl_pmic5.c
+++ drivers/thermal/qcom/bcl_pmic5.c
@@ -67,6 +67,7 @@
 #define MAX_PERPH_COUNT       2
 #define IPC_LOGPAGES          2
 
+#ifdef CONFIG_IPC_LOGGING
 #define BCL_IPC(dev, msg, args...)      do { \
 			if ((dev) && (dev)->ipc_log) { \
 				ipc_log_string((dev)->ipc_log, \
@@ -74,6 +75,9 @@
 					current->comm, __func__, args); \
 			} \
 		} while (0)
+#else
+#define BCL_IPC(...) ((void)0)
+#endif
 
 enum bcl_dev_type {
 	BCL_IBAT_LVL0,
@@ -802,7 +806,9 @@ static int bcl_remove(struct platform_device *pdev)
 static int bcl_probe(struct platform_device *pdev)
 {
 	struct bcl_device *bcl_perph = NULL;
+#ifdef CONFIG_IPC_LOGGING
 	char bcl_name[40];
+#endif
 	int err = 0;
 
 	if (bcl_device_ct >= MAX_PERPH_COUNT) {
@@ -835,6 +841,7 @@ static int bcl_probe(struct platform_device *pdev)
 
 	dev_set_drvdata(&pdev->dev, bcl_perph);
 
+#ifdef CONFIG_IPC_LOGGING
 	snprintf(bcl_name, sizeof(bcl_name), "bcl_0x%04x_%d",
 					bcl_perph->fg_bcl_addr,
 					bcl_device_ct - 1);
@@ -844,6 +851,7 @@ static int bcl_probe(struct platform_device *pdev)
 	if (!bcl_perph->ipc_log)
 		pr_err("%s: unable to create IPC Logging for %s\n",
 					__func__, bcl_name);
+#endif
 
 	return 0;
 }
