c1219d25bdb0bd9d7ed20029bda6874f549904da
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 07:53:41 +0000

    Revert "sched: avoid scheduling RT threads on cores currently handling softirqs"
    This reverts commit 6e250f90ece87217e32ec23def6022a39ac24eb7.
    
    Change-Id: Ife8f6540f9276d61174569669e951b640ada6515
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -7,8 +7,6 @@
 
 #include "pelt.h"
 
-#include <linux/interrupt.h>
-
 #include <trace/events/sched.h>
 
 #include "walt/walt.h"
@@ -1539,20 +1537,6 @@ static void yield_task_rt(struct rq *rq)
 #ifdef CONFIG_SMP
 static int find_lowest_rq(struct task_struct *task);
 
-/*
- * Return whether the task on the given cpu is currently non-preemptible
- * while handling a softirq or is likely to block preemptions soon because
- * it is a ksoftirq thread.
- */
-bool
-task_may_not_preempt(struct task_struct *task, int cpu)
-{
-	struct task_struct *cpu_ksoftirqd = per_cpu(ksoftirqd, cpu);
-
-	return (task_thread_info(task)->preempt_count & SOFTIRQ_MASK) ||
-	       task == cpu_ksoftirqd;
-}
-
 static int
 #ifdef CONFIG_SCHED_WALT
 select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags,
@@ -1564,7 +1548,6 @@ select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags)
 	struct task_struct *curr;
 	struct rq *rq;
 	struct rq *this_cpu_rq;
-	bool may_not_preempt;
 	bool test;
 	bool sync = !!(flags & WF_SYNC);
 	int this_cpu;
@@ -1587,17 +1570,7 @@ select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags)
 	this_cpu_rq = cpu_rq(this_cpu);
 
 	/*
-	 * If the current task on @p's runqueue is a softirq task,
-	 * it may run without preemption for a time that is
-	 * ill-suited for a waiting RT task. Therefore, try to
-	 * wake this RT task on another runqueue.
-	 *
-	 * Also, if the current task on @p's runqueue is an RT task, then
-	 * it may run without preemption for a time that is
-	 * ill-suited for a waiting RT task. Therefore, try to
-	 * wake this RT task on another runqueue.
-	 *
-	 * Also, if the current task on @p's runqueue is an RT task, then
+	 * If the current task on @p's runqueue is an RT task, then
 	 * try to see if we can wake this RT task up on another
 	 * runqueue. Otherwise simply start this RT task
 	 * on its current runqueue.
@@ -1622,7 +1595,6 @@ select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags)
 	 * requirement of the task - which is only important on heterogeneous
 	 * systems like big.LITTLE.
 	 */
-	may_not_preempt = task_may_not_preempt(curr, cpu);
 	test = curr &&
 	       unlikely(rt_task(curr)) &&
 	       (curr->nr_cpus_allowed < 2 || curr->prio <= p->prio);
@@ -1636,7 +1608,7 @@ select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags)
 		goto out_unlock;
 	}
 
-	if (sched_energy_enabled() || may_not_preempt ||
+	if (sched_energy_enabled() ||
 	    test || !rt_task_fits_capacity(p, cpu)) {
 		int target = find_lowest_rq(p);
 
@@ -1648,14 +1620,11 @@ select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags)
 			goto out_unlock;
 
 		/*
-		 * If cpu is non-preemptible, prefer remote cpu
-		 * even if it's running a higher-prio task.
-		 * Otherwise: Don't bother moving it if the
-		 * destination CPU is not running a lower priority task.
+		 * Don't bother moving it if the destination CPU is
+		 * not running a lower priority task.
 		 */
 		if (target != -1 &&
-		    (may_not_preempt ||
-		     p->prio < cpu_rq(target)->rt.highest_prio.curr))
+		    p->prio < cpu_rq(target)->rt.highest_prio.curr)
 			cpu = target;
 	}
 
