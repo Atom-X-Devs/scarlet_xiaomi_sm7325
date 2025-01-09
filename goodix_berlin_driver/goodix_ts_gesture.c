a9516818917ced04402d1ce928e1773c8da4cb82
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-01-10 01:06:46 +0600

    touch: goodix: Set KEY_GOTO for single tap gesture
    KEY_WAKEUP event for single tap gesture always wakes up the screen when
    screen is blank which is unwanted. The correct apporach is to map it to
    KEY_GOTO so that userspace can handle single tap gesture (if needed).
    As of now, only double tap gesture should wake up the screen.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- goodix_berlin_driver/goodix_ts_gesture.c
+++ goodix_berlin_driver/goodix_ts_gesture.c
@@ -313,9 +313,9 @@ static int gsx_gesture_ist(struct goodix_ts_core *cd,
 	case GOODIX_GESTURE_SINGLE_TAP:
 		if (cd->gesture_type & GESTURE_SINGLE_TAP) {
 			ts_info("get SINGLE-TAP gesture");
-			input_report_key(cd->input_dev, KEY_WAKEUP, 1);
+			input_report_key(cd->input_dev, KEY_GOTO, 1);
 			input_sync(cd->input_dev);
-			input_report_key(cd->input_dev, KEY_WAKEUP, 0);
+			input_report_key(cd->input_dev, KEY_GOTO, 0);
 			input_sync(cd->input_dev);
 		}
 		break;
