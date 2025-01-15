Atom-X-Devs/scarlet_xiaomi_sm7325

commit adfef75542f05cedc8dfdc19ce1fb5928c938d35

touch: goodix: Fix data racing
Bug: 229036434
Test: Be able to get fw version
Change-Id: Idd2f8701fa7b984564f9df379f49eb23c8015b19
Signed-off-by: Wendly Li <wendlyli@google.com>
Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

goodix_berlin_driver/goodix_brl_spi.c
@@ -55,6 +55,8 @@ static int goodix_spi_read_bra(struct device *dev, unsigned int addr,
 	int ret = 0;
 	int buf_len = SPI_READ_PREFIX_LEN + len;
 
+	mutex_lock(&goodix_spi_bus.mutex);
+
 	if (buf_len <= SPI_PREALLOC_RX_BUF_SIZE &&
 		buf_len <= SPI_PREALLOC_TX_BUF_SIZE) {
 		rx_buf = goodix_spi_bus.rx_buf;
@@ -101,6 +103,8 @@ static int goodix_spi_read_bra(struct device *dev, unsigned int addr,
 	}
 	memcpy(data, &rx_buf[SPI_READ_PREFIX_LEN], len);
 
+	mutex_unlock(&goodix_spi_bus.mutex);
+
 err_spi_transfer:
 	if (rx_buf != goodix_spi_bus.rx_buf)
 		kfree(rx_buf);
@@ -120,6 +124,8 @@ static int goodix_spi_read(struct device *dev, unsigned int addr,
 	struct spi_message spi_msg;
 	int ret = 0;
 	int buf_len = SPI_READ_PREFIX_LEN - 1 + len;
+
+	mutex_lock(&goodix_spi_bus.mutex);
 
 	if (buf_len <= SPI_PREALLOC_RX_BUF_SIZE &&
 		buf_len <= SPI_PREALLOC_TX_BUF_SIZE) {
@@ -166,6 +172,8 @@ static int goodix_spi_read(struct device *dev, unsigned int addr,
 	}
 	memcpy(data, &rx_buf[SPI_READ_PREFIX_LEN - 1], len);
 
+	mutex_unlock(&goodix_spi_bus.mutex);
+
 err_spi_transfer:
 	if (rx_buf != goodix_spi_bus.rx_buf)
 		kfree(rx_buf);
@@ -206,6 +214,8 @@ static int goodix_spi_write(struct device *dev, unsigned int addr,
 	spi_message_init(&spi_msg);
 	memset(&xfers, 0, sizeof(xfers));
 
+	mutex_lock(&goodix_spi_bus.mutex);
+
 	tx_buf[0] = SPI_WRITE_FLAG;
 	tx_buf[1] = (addr >> 24) & 0xFF;
 	tx_buf[2] = (addr >> 16) & 0xFF;
@@ -217,6 +227,9 @@ static int goodix_spi_write(struct device *dev, unsigned int addr,
 	xfers.cs_change = 0;
 	spi_message_add_tail(&xfers, &spi_msg);
 	ret = spi_sync(spi, &spi_msg);
+
+	mutex_unlock(&goodix_spi_bus.mutex);
+
 	if (ret < 0)
 		ts_err("spi transfer error:%d", ret);
 
@@ -272,6 +285,8 @@ static int goodix_spi_probe(struct spi_device *spi)
 		ret = -ENOMEM;
 		goto err_alloc_tx_buf;
 	}
+
+	mutex_init(&goodix_spi_bus.mutex);
 
 	/* ts core device */
 	goodix_pdev = kzalloc(sizeof(struct platform_device), GFP_KERNEL);

goodix_berlin_driver/goodix_ts_core.h
@@ -430,6 +430,7 @@ struct goodix_bus_interface {
 	struct device *dev;
 	u8 *rx_buf;
 	u8 *tx_buf;
+	struct mutex mutex;
 	int (*read)(struct device *dev, unsigned int addr,
 			 unsigned char *data, unsigned int len);
 	int (*write)(struct device *dev, unsigned int addr,

commit b6a2f8159a20365da4233b39caec447126becb1f

touch: goodix: Destroy mutex upon exiting
Mutex was never destroyed after exiting probe. Fix it.

Fixes: adfef75 ("touch: goodix: Fix data racing")
Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

goodix_berlin_driver/goodix_brl_spi.c
@@ -331,6 +331,7 @@ err_alloc_tx_buf:
 static int goodix_spi_remove(struct spi_device *spi)
 {
 	platform_device_unregister(goodix_pdev);
+	mutex_destroy(&goodix_spi_bus.mutex);
 	return 0;
 }
 