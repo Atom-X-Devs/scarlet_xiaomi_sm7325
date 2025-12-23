1635d0404531fa2adcc51982cd8e3dfe35abe3f9
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-12-23 22:29:52 +0600

    Revert "BACKPORT: UPSTREAM: sched: Move psi_account_irqtime() out of update_rq_clock_task() hotpath"
    Commit "sched/psi: Cache parent psi_group to speed up group iteration" of this commit's patchset
    "BACKPORT: sched/psi: Add PSI_IRQ to track IRQ/SOFTIRQ pressure" breaks boot when PSI is enabled.
    
    In order to revert the patchset, revert this.
    
    This reverts commit a7e0142ef54b8d1041ad078378bd2dff17156f54.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -617,6 +617,7 @@ static void update_rq_clock_task(struct rq *rq, s64 delta)
 
 	rq->prev_irq_time += irq_delta;
 	delta -= irq_delta;
+	psi_account_irqtime(rq->curr, irq_delta);
 #endif
 #ifdef CONFIG_PARAVIRT_TIME_ACCOUNTING
 	if (static_key_false((&paravirt_steal_rq_enabled))) {
@@ -5220,16 +5221,13 @@ void scheduler_tick(void)
 {
 	int cpu = smp_processor_id();
 	struct rq *rq = cpu_rq(cpu);
-	struct task_struct *curr;
+	struct task_struct *curr = rq->curr;
 	struct rq_flags rf;
 	unsigned long thermal_pressure;
 
 	sched_clock_tick();
 
 	rq_lock(rq, &rf);
-
-	curr = rq->curr;
-	psi_account_irqtime(rq, curr, NULL);
 
 	update_rq_clock(rq);
 	thermal_pressure = arch_scale_thermal_pressure(cpu_of(rq));
@@ -6271,7 +6269,6 @@ picked:
 		++*switch_count;
 
 		migrate_disable_switch(rq, prev);
-		psi_account_irqtime(rq, prev, next);
 		psi_sched_switch(prev, next, !task_on_rq_queued(prev) ||
 					     prev->se.sched_delayed);
 
