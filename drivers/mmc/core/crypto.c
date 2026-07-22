eb2854923297398c770d2d92f547021fe00d4886
Harukiansyah <119049542+Harukiansyah@users.noreply.github.com>
2026-07-23 00:27:07 +0700

    Update crypto.c

--- drivers/mmc/core/crypto.c
+++ drivers/mmc/core/crypto.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
-/*
- * Copyright 2020 Google LLC
+/* 
+ * Di-sunat paksa buat bypass error Neutron Clang.
+ * Redwood pake UFS, MMC Crypto kaga guna dimari!
  */
 
 #include <linux/blk-crypto.h>
@@ -11,30 +12,19 @@
 #include "core.h"
 #include "queue.h"
 
-void mmc_crypto_setup_queue(struct mmc_host *host, struct request_queue *q)
+void mmc_crypto_setup_queue(struct mmc_host *host, struct request_queue *q) 
 {
-	if (host->caps2 & MMC_CAP2_CRYPTO)
-		q->ksm = host->ksm;
+    // Dikosongin biar Clang ga cerewet
 }
 EXPORT_SYMBOL_GPL(mmc_crypto_setup_queue);
 
-void mmc_crypto_free_host(struct mmc_host *host)
+void mmc_crypto_free_host(struct mmc_host *host) 
 {
-	keyslot_manager_destroy(host->ksm);
+    // Dikosongin
 }
 
-void mmc_crypto_prepare_req(struct mmc_queue_req *mqrq)
+void mmc_crypto_prepare_req(struct mmc_queue_req *mqrq) 
 {
-	struct request *req = mmc_queue_req_to_req(mqrq);
-	struct mmc_request *mrq = &mqrq->brq.mrq;
-	const struct bio_crypt_ctx *bc;
-
-	if (!bio_crypt_should_process(req))
-		return;
-
-	bc = req->bio->bi_crypt_context;
-	mrq->crypto_key_slot = bc->bc_keyslot;
-	mrq->data_unit_num = bc->bc_dun[0];
-	mrq->crypto_key = bc->bc_key;
+    // Dikosongin
 }
 EXPORT_SYMBOL_GPL(mmc_crypto_prepare_req);
