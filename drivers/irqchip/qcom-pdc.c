7fb60029fc59e49fc386153f69a0a5d6172100f4
Divyanshu-Modi <divyan.m05@gmail.com>
2026-09-09 22:47:06 +0530

    Revert "drivers: irqchip: qcom-pdc: Add PDC IRQ chip support for shima"
    This reverts commit 4acf052c6eb57e65d88bb2a93ce61cb862ecef1d.
    
    Change-Id: I7d71d95bc55c7f3e16e69ff9dfd87d4836e90d27
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/irqchip/qcom-pdc.c
+++ drivers/irqchip/qcom-pdc.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
  */
 
 #include <linux/err.h>
@@ -569,7 +569,6 @@ module_platform_driver(qcom_pdc_driver);
 #else
 IRQCHIP_DECLARE(qcom_pdc, "qcom,pdc", qcom_pdc_init);
 IRQCHIP_DECLARE(pdc_lahaina, "qcom,lahaina-pdc", qcom_pdc_init);
-IRQCHIP_DECLARE(pdc_shima, "qcom,shima-pdc", qcom_pdc_init);
 #endif
 
 MODULE_DESCRIPTION("Qualcomm Technologies, Inc. Power Domain Controller");
